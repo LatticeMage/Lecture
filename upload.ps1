fdd17d3ec36cba86ccdf4e587c369e58f6c2d363
LatticeMage <LatticeMage@users.noreply.github.com>
2023-08-08 16:15:47 +0800

    upload

--- upload.ps1
+++ upload.ps1
@@ -1,12 +1,4 @@
-git config --local user.name "LatticeMage"
-git config --local user.email "LatticeMage@users.noreply.github.com"
-git remote set-url origin git@LM:LatticeMage/Lecture.git
-
-git pull
-git add .
-git commit -m "upload"
-git push
-
+./upload.bat
 
 cd ../LatticeMage.github.io/
 ./upload.ps1
